8ac902ad3b449ab4d995dd050c8cd9cc466a8041
Bodo <admin.nxpoint@gmail.com>
2024-05-13 18:02:10 +0300

    remove id

--- aws/resources/aws_db_subnet_group/main.tf
+++ aws/resources/aws_db_subnet_group/main.tf
@@ -1,6 +1,5 @@
 resource "aws_db_subnet_group" "r_aws_db_subnet_group" {
   description = var.description
-  id          = var.id
   name        = var.name
   name_prefix = var.name_prefix
   subnet_ids  = var.subnet_ids
